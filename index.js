8b9cc14946c84a613ff59513933252c7b3bbe8a5
matthew rendely <matthew.rendely@gmail.com>
2023-02-20 13:43:59 -0800

    done

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+  const input_button = document.querySelector('input#button');
+  input_button .addEventListener('click', function () {
+    alert('I was clicked!');
+  });
 }
+
+
+addingEventListener();
